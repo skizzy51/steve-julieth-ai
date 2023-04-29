a7511e7c6212d644654719ebe78b7077da8b5916
skizzy51 <obisteve51@gmail.com>
2023-04-29 10:05:12 +0100

    made media responsive

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 "use client"
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import styles from "../styles/Home.module.scss"
 import { AnimatePresence, motion } from "framer-motion"
 
